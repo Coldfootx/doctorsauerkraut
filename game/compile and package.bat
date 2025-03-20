fef229bc6108a877b0f567721f238767f6edcf03
Nimitzf <korpimail@gmail.com>
2025-03-20 12:19:31 +0200

    works from box
    create folders when necessary

--- game/compile and package.bat
+++ game/compile and package.bat
@@ -1,4 +1,5 @@
 call compile.bat
 
+mkdir .\package
 powershell Compress-Archive .\compiled\game\* -Force .\package\game.zip
 REM powershell Compress-Archive .\compiled\server\* .\package\server.zip
