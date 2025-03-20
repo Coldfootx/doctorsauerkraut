daa5a38768406c8210040f924aba425b298b4e5a
Nimitzf <korpimail@gmail.com>
2025-03-20 20:00:08 +0200

    työstön alla

--- game/source/game/main.lua
+++ game/source/game/main.lua
@@ -1,27 +1,35 @@
 MAPW = 1024
 MAPH = 1024
+WINDOWW = 1000
+WINDOWH = 800
 
-local map = {}
-for i=1,MAPW do
-    map[i] = {}     -- create x
-    for j=1,MAPH do
-        map[i][j] = 0
+do 
+    function love.load()
+        -- image = love.graphics.newImage("assets/love-ball.png")
+
+        buffer = require("string.buffer")
+
+        map = {}
+        for i=1,MAPW do
+            map[i] = {}     -- create x
+            for j=1,MAPH do
+                map[i][j] = 0
+            end
+        end
+
+        save = {map, 5, "aac"}
+
+        local encoded_tbl = buffer.encode(save)
+        
+        --love.data.encode("base64", "string", str))
+
+        save = buffer.decode(encoded_tbl)
+
+        -- love.graphics.setColor(0,1,0)
+    end
+    
+    function love.draw()
+       --  love.graphics.draw(image, 400, 300)
+       love.graphics.printf(tostring(save[1][1])..tostring(save[2])..tostring(save[3]), 0, WINDOWH-50, WINDOWW)
     end
 end
-
-local save = {map}
-
-local datastream = love.data.pack("data", "I", save)
-
-local FilePath = SKIN:MakePathAbsolute('./save/save1')
-local File = io.open(FilePath, 'w')
-File:write(datastream)
-File:close()
-
-local File = io.open(FilePath, 'r')
-datastream = io.read()
-save = love.data.unpack("I", datastream, 1)
-
-love.graphics.print(save[1][1]+" "+save[1][1]+" "+save[2][2], 400, 300)
-
-
