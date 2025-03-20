6df1b8c58464ca10da60be7d8ecc9b109be95fe0
Nimitzf <korpimail@gmail.com>
2025-03-20 13:08:39 +0200

    no server files for no reason

--- game/compile and package.bat
+++ game/compile and package.bat
@@ -2,4 +2,3 @@ call compile.bat
 
 mkdir .\package
 powershell Compress-Archive .\compiled\game\* -Force .\package\game.zip
-REM powershell Compress-Archive .\compiled\server\* .\package\server.zip
--- game/source/server/conf.lua
+++
@@ -1,8 +0,0 @@
-function love.conf(t)
-	t.modules.window = false
-	t.modules.graphics = false
-	t.console = true
-
-	t.modules.joystick = false
-	t.modules.physics = false
-end
