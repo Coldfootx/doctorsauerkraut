5aa9fd2d92b8c3f60398f7707e3d8b43de1f4a43
Nimitzf <korpimail@gmail.com>
2025-03-20 16:36:59 +0200

    tutorial crap cleaned

--- game/source/game/conf.lua
+++
@@ -1,8 +0,0 @@
-love.conf = function(t)
-	t.identity = "cimgui-love-test"
-	t.window.resizable = true
-	t.window.width = 1280
-	t.window.height = 720
-
-	t.window.title = "Old Nite"
-end
--- game/source/game/main.lua
+++ game/source/game/main.lua
@@ -1,3 +1,42 @@
+MAPW = 1024
+MAPH = 1024
+
+map = {}
+for i=1,MAPW do
+    map[i] = {}     -- create x
+    for j=1,MAPH do
+        map[i][j] = 0
+    end
+end
+
+local tab = {map}
+
+local t = {}
+for n = 1, ITEMS do
+    t[n] = {}
+end
+
+local time = os.clock()
+for n = 1, ITEMS do
+    table.insert(tab, t[n])
+end
+print(('table.insert: %.4f'):format(os.clock() - time))
+
+local time = os.clock()
+for n = ITEMS, 1, -2 do
+    table.remove(tab, n)
+end
+print(('table.remove: %.4f'):format(os.clock() - time))
+
+local time = os.clock()
+for m = 1, ITER do
+    for n = 1, #tab do
+        assert(tab[n])
+    end
+end
+    print(('table iterate: %.4f'):format(os.clock() - time))
+
 function love.draw()
     love.graphics.print("Hello World", 400, 300)
 end
+
