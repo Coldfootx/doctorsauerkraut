c03a935a9fd21f56eb4233700ddf4fb5d9f66f18
Nimitzf <korpimail@gmail.com>
2025-03-21 10:39:17 +0200

    working on serializing savefiles

--- game/source/game/main.lua
+++ game/source/game/main.lua
@@ -5,9 +5,18 @@ WINDOW_H = 600
 SAVEFILE_NAME = "savefile"
 
 do 
-    function print_to_debug(text)
+    local love = require("love")
+    local lume = require("lume")
+    
+    local function print_to_debug(text)
         love.graphics.setColor(0,1,0)
         love.graphics.printf(text, 10, WINDOW_H-45, WINDOW_W)
+
+        love.graphics.setColor(0,0,0)
+        love.graphics.printf(text, 10+1, WINDOW_H-45, WINDOW_W)
+        love.graphics.printf(text, 10-1, WINDOW_H-45, WINDOW_W)
+        love.graphics.printf(text, 10, WINDOW_H-45+1, WINDOW_W)
+        love.graphics.printf(text, 10, WINDOW_H-45-1, WINDOW_W)
     end
 
     function love.load()
@@ -16,40 +25,36 @@ do
         love.window.setMode(WINDOW_W, WINDOW_H)
 
         local buffer = require("string.buffer")
+        local lume = require("lume.lib")
 
-        MAP = {}
+        Map = {}
         for i=1,MAP_W do
-            MAP[i] = {}     -- create x
+            Map[i] = {}     -- create x
             for j=1,MAP_H do
-                MAP[i][j] = 0
+                Map[i][j] = 0
             end
         end
 
-        SAVE = {MAP, 5, "aac"}
+        Save = {Map, 5, "aac"}
 
-        local f = assert(io.open(SAVEFILE_NAME, "wb"))
-        f:write(buffer.encode(SAVE))
-        f:close()
+        local serialized
 
-        f = assert(io.open(SAVEFILE_NAME, "rb"))
-        SAVE = buffer.decode(f:read())
-        f:close()
-
-
-        local encoded_tbl = buffer.encode(SAVE)
+        for i=1,MAP_W do
+            for j=1,MAP_H do
+                serialized = serialized .. table.concat(Save[1])
+            end
+        end
         
-        --love.data.encode("base64", "string", str))
+        local file = assert(io.open(SAVEFILE_NAME, "w"))
+        file:write(serialized)
+        file:close()
 
-        SAVE = buffer.decode(encoded_tbl)
-        
-        --love.data.encode("base64", "string", str))
-
-
-        -- love.graphics.setColor(0,1,0)
-    end
+        local file = assert(io.open(SAVEFILE_NAME, "r"))
+        serialized = io.read()
+        file:close()
     
     function love.draw()
         --  love.graphics.draw(image, 400, 300)
-        print_to_debug(tostring(tonumber(SAVE[1][1]))..", "..tostring(SAVE[2])..", "..tostring(SAVE[3]))
+        print_to_debug(tostring(tostring(Save[1][2][5]))..", "..tostring(Save[2])..", "..tostring(Save[3]))
     end
 end
