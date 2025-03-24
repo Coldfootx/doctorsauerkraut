4811722cfa0a4b3d053a7fd088e8bae3e84f6eff
Nimitzf <korpimail@gmail.com>
2025-03-24 10:03:42 +0200

    working with menus

--- game/source/game/main.lua
+++ game/source/game/main.lua
@@ -27,6 +27,8 @@ do
     local love = require("love")
     local lume = require("lib.lume")
 
+    local gfx = love.graphics
+
     local choice
     if love.filesystem.getInfo(RANDOMNESSFILE) == nil then
         love.filesystem.write(RANDOMNESSFILE, tostring(STARTING_RANDOMNESS))
@@ -54,23 +56,52 @@ do
         Save = lume.deserialize(love.data.decompress("string", COMPRESSION, contents))
     end
 
+    local function table_len(t)
+        local n = 0
+        for _ in pairs(t) do
+            n = n + 1
+        end
+        return n
+    end
+
     local function quitmessage()
-        local pressedbutton = love.window.showMessageBox("Want to Quit?", "All unsaved progress will be lost", {"OK", "No!", escapebutton = 1}, "warning", true)
+        local pressedbutton = love.window.showMessageBox("Want to Quit?", "All unsaved progress will be lost", {"OK", "No!", enterbutton = 2}, "warning", true)
         if pressedbutton == 1 then
             love.event.quit()
         end
     end
 
+    local function savegame()
+
+    end
+
+    local function newgame()
+        love.window.showMessageBox("New Game", "Starting a new game", {"OK"}, "info", true)
+    end
+
+    local function loadgame()
+
+    end
+
+    local function helpwindow()
+
+    end
+
+    local function quitgame()
+        quitmessage()
+    end
+
     function love.keypressed(key, scancode, isrepeat)
-        if key == "escape" and State.leaf ~= 0 then
-            State.leaf = 0
+        if key == "escape" and State.leaf ~= 1 then
+            State.oldleaf = State.leaf
+            State.leaf = 1
         elseif key == "escape" then
-            quitmessage()
+            quitgame()
         end
     end
 
     local function translatexy(x1, y1)
-        local width, height = love.graphics.getDimensions( )
+        local width, height = gfx.getDimensions( )
         x1 = x1*width
         y1 = y1*height
         return x1, y1
@@ -78,8 +109,8 @@ do
 
     local function print_to_debug(text)
         local width, height = translatexy(0.01, 0.95)
-        love.graphics.setColor(0,1,0)
-        love.graphics.print(text, width, height)
+        gfx.setColor(0,1,0)
+        gfx.print(text, width, height)
     end
 
     local function generate_map()
@@ -97,28 +128,22 @@ do
 
         return maptotal
     end
-
-    local function savegame()
-
-    end
-
-    local function newgame()
-
-    end
-
-    local function loadgame()
-
-    end
-
-    local function helpwindow()
-
-    end
-
-    local function quitgame()
-
-    end
-
+    
     function love.mousepressed(x, y, mouse_button, is_touch)
+        Buttons[State.leaf][State.hoover].call()
+        Randomseed = Randomseed+1
+    end
+
+    function love.mousemoved( x, y, dx, dy, istouch )
+        if State.leaf == 1 then
+            local len = table_len(Buttons[State.leaf])
+            for i=1,len do
+                if x > Buttons[State.leaf][i].x and x < Buttons[State.leaf][i].x + Buttons[State.leaf][i].width and y > Buttons[State.leaf][i].y and y <Buttons[State.leaf][i].y + Buttons[State.leaf][i].height then
+                    State.hoover = i
+                    break
+                end
+            end
+        end
     end
 
     function love.load()
@@ -138,9 +163,23 @@ do
 
         --generate all data
         MapTotal = generate_map()
+        local newgamebuttonh = 50
+        local newgamebuttonw = 300
+        local newbuttonstarth = 200
+        local newgamebuttonpadding = 10
+        Buttons = { 
+            {
+                {text="New Game", x = WINDOW_W/2.0-newgamebuttonw/2.0, y = newbuttonstarth, width = newgamebuttonw, height=newgamebuttonh, call = newgame},{text="Save Game", x = WINDOW_W/2.0-newgamebuttonw/2.0, y = newbuttonstarth+newgamebuttonh+newgamebuttonpadding, width = newgamebuttonw, height=newgamebuttonh, call = savegame}, {text="Load Game", x = WINDOW_W/2.0-newgamebuttonw/2.0, y = newbuttonstarth+2*newgamebuttonh+2*newgamebuttonpadding, width = newgamebuttonw, height=newgamebuttonh, call = loadgame}, {text="Help", x = WINDOW_W/2.0-newgamebuttonw/2.0, y = newbuttonstarth+3*newgamebuttonh+3*newgamebuttonpadding, width = newgamebuttonw, height=newgamebuttonh, call = helpwindow}, {text="Quit", x = WINDOW_W/2.0-newgamebuttonw/2.0, y = newbuttonstarth+4*newgamebuttonh+4*newgamebuttonpadding, width = newgamebuttonw, height=newgamebuttonh, call = quitgame}
+                },
+                {
+            },
+            {
 
-        State = {leaf = 0}
-        -- leaf 0 = main menu, 1 = new game,
+            }
+        }
+        
+        State = {leaf = 1, oldleaf = 1, hoover = 0}
+        -- leaf 1 = main menu, 2 = new game,
     end
 
     function love.update(dt)
@@ -152,10 +191,24 @@ do
     end
 
     function love.draw()
-        if State.leaf == 0 then
+        if State.leaf == 1 then
+            gfx.setColor(1,1,1)
+            local len = table_len(Buttons[State.leaf])
+            
+            for i=1,len do
+                if State.hoover == i then
+                    gfx.setColor(0,0,0)
+                    gfx.rectangle("fill", Buttons[State.leaf][i].x, Buttons[State.leaf][i].y, Buttons[State.leaf][i].width, Buttons[State.leaf][i].height)
+                    gfx.setColor(1,1,1)
+                    gfx.rectangle("line", Buttons[State.leaf][i].x, Buttons[State.leaf][i].y, Buttons[State.leaf][i].width, Buttons[State.leaf][i].height)
+                else
+                    gfx.setColor(1,1,1)
+                    gfx.rectangle("fill", Buttons[State.leaf][i].x, Buttons[State.leaf][i].y, Buttons[State.leaf][i].width, Buttons[State.leaf][i].height)
+                end
+            end
 
         end
-        local width, height = love.graphics.getDimensions()
+        local width, height = gfx.getDimensions()
         print_to_debug(width.."x"..height..", vsync="..love.window.getVSync()..", fps="..love.timer.getFPS()..", mem="..string.format("%.3f", collectgarbage("count")/1000.0).."MB, mapnumber="..MapTotal..", randomseed="..Randomseed)
     end
 end
